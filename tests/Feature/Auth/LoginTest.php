80a02894055abe3a68fae59eedfc89dce9adc9a1
Obare C. Brian <brianobare@gmail.com>
2020-04-19 16:00:24 +0300

    authentication and tests

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+use Session;
+
+class LoginTest extends TestCase
+{
+    public function testUserCanViewLoginForm()
+    {
+        $response = $this->get('/login');
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
+
+    public function testUserCannotViewLoginFormWhenAuthenticated()
+    {
+        $user = factory(User::class)->make();
+
+        $response = $this->actingAs($user)->get('/login');
+
+        $response->assertRedirect('/home');
+    }
+
+    public function testUserCanLoginWithCorrectCredentials()
+    {
+        Session::start();
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+            '_token' => csrf_token()
+        ]);
+
+        $response->assertRedirect('/home');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function testUserCannotLoginWithIncorrectPassword()
+    {
+        Session::start();
+        $user = factory(User::class)->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid',
+            '_token' => csrf_token()
+        ]);
+
+        $response->assertSessionHasErrors();
+
+        $this->assertGuest();
+    }
+
+    public function testRememberMeFunctionality()
+    {
+        Session::start();
+        $user = factory(User::class)->create([
+            'id' => random_int(1, 100),
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+        
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+            'remember' => 'on',
+            '_token' => csrf_token()
+        ]);
+        
+        $response->assertRedirect('/home');
+        // cookie assertion goes here
+        $this->assertAuthenticatedAs($user);
+    }
+}
---
+++ tests/Feature/Auth/RegisterTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use Session;
+
+use App\User;
+
+class RegisterTest extends TestCase
+{
+    public function testRegisterFormDisplayed()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    public function testRegisterValidUser()
+    {
+        Session::start();
+        $user = factory(User::class)->make();
+
+        $response = $this->post('register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            '_token' => csrf_token()
+        ]);
+
+        $response->assertStatus(302);
+
+        $this->assertAuthenticated();
+    }
+
+    public function testDoesNotRegisterInvalidUser()
+    {
+        Session::start();
+        $user = factory(User::class)->make();
+
+        $response = $this->post('register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'invalid',
+            '_token' => csrf_token()
+        ]);
+
+        $response->assertSessionHasErrors();
+
+        $this->assertGuest();
+    }
+}
