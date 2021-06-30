e4f8a80c24d820fab9d284d1c14b6169dd66e60c
ereng <aereng@gmail.com>
2021-06-30 16:51:23 +0300

    Added Github token Form

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,21 +1,46 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">This is home</div>
-          <div class="card-body">
-              This is home
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<div>
+  <b-form @submit="onSubmit" v-if="show" inline>
+    <label class="mr-sm-2" for="inline-form-input-name">GitHub Token</label>
+    <b-form-input
+      id="inline-form-input-name"
+      v-model="form.git_hub_token"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      placeholder=""
+    ></b-form-input>
+      <!-- <b-button type="submit" variant="primary">Submit</b-button> -->
+    <b-button type="submit" variant="primary">Save</b-button>
+  </b-form>
+</div>
 </template>
 <script>
 export default {
   mounted() {
     console.log('Component mounted.')
+  },
+  data() {
+    return {
+      form: {
+        git_hub_token: '',
+      },
+      show: true
+    }
+  },
+  created () {
+    this.initialize()
+  },
+  methods: {
+    initialize () {
+      axios.get('/githubtoken', this.form).then(response => {
+        this.form.git_hub_token = response.data.git_hub_token;
+      }).catch(error => console.log(error));
+    },
+    onSubmit(event) {
+      event.preventDefault()
+      axios.post('/githubtoken', this.form).then(response => {
+        this.form.git_hub_token = response.data.git_hub_token;
+      }).catch(error => console.log(error));
+    }
   }
 }
 </script>
